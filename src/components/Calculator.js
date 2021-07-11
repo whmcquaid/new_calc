7d7a2dd0ac9f574a38cec044bafd5ee06e442522
William McQuaid <whmcquaid@gmail.com>
2021-07-11 18:11:24 -0400

    added function buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import NumberButton from './NumberButton';
-// import FunctionButton from './FunctionButton';
+gimport FunctionButton from './FunctionButton';
 import ClearButton from './ClearButton';
 import Display from './Display';
 // import EqualButton from './EqualButton';
@@ -18,24 +18,24 @@ const Calculator = () => (
       <ClearButton />
       <BackButton />
      <NegativeButton />
-      {/* <FunctionButton buttonValue="/" />
-      <NumberButton buttonValue={7} />
+       <FunctionButton buttonValue="/" />
+      {/* <NumberButton buttonValue={7} />
       <NumberButton buttonValue={8} />
-      <NumberButton buttonValue={9} />
+      <NumberButton buttonValue={9} /> */}
       <FunctionButton buttonValue="*" />
-      <NumberButton buttonValue={4} />
+      {/* <NumberButton buttonValue={4} />
       <NumberButton buttonValue={5} />
-      <NumberButton buttonValue={6} />
+      <NumberButton buttonValue={6} /> */}
       <FunctionButton buttonValue="-" />
-      <NumberButton buttonValue={1} />
-      <NumberButton buttonValue={2} />
-      <NumberButton buttonValue={3} />
+      {/* <NumberButton buttonValue={1} />
+      <NumberButton buttonValue={2} /> */}
+      {/* <NumberButton buttonValue={3} /> */}
       <FunctionButton buttonValue="+" />
-      <div className="zero-button">
+      {/* <div className="zero-button">
         <NumberButton buttonValue={0} />
-      </div>
-      <NumberButton buttonValue="." />
-      <EqualButton /> */}
+      </div> */}
+      {/* <NumberButton buttonValue="." /> */}
+      {/* <EqualButton /> */}
     </div>
   </CalculatorStyles>
 );
