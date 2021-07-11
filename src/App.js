2c4733770972d8f42952ec33408153de76c9fdc6
William McQuaid <whmcquaid@gmail.com>
2021-07-11 17:37:26 -0400

    testing number provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NumberProvider from './components/NumberProvider';
 
 function App() {
   return (
+    <NumberProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Testing Number Provider
     </div>
+    </NumberProvider>
   );
 }
 
