6f46278cbfee87c96e7ccc90fd13038ea5c9c51d
William McQuaid <whmcquaid@gmail.com>
2021-07-11 18:16:51 -0400

    added number buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import NumberButton from './NumberButton';
+import NumberButton from './NumberButton';
 import FunctionButton from './FunctionButton';
 import ClearButton from './ClearButton';
 import Display from './Display';
@@ -19,23 +19,22 @@ const Calculator = () => (
       <BackButton />
      <NegativeButton />
        <FunctionButton buttonValue="/" />
-      {/* <NumberButton buttonValue={7} />
+      <NumberButton buttonValue={7} />
       <NumberButton buttonValue={8} />
-      <NumberButton buttonValue={9} /> */}
+      <NumberButton buttonValue={9} />
       <FunctionButton buttonValue="*" />
-      {/* <NumberButton buttonValue={4} />
+      <NumberButton buttonValue={4} />
       <NumberButton buttonValue={5} />
-      <NumberButton buttonValue={6} /> */}
+      <NumberButton buttonValue={6} />
       <FunctionButton buttonValue="-" />
-      {/* <NumberButton buttonValue={1} />
-      <NumberButton buttonValue={2} /> */}
-      {/* <NumberButton buttonValue={3} /> */}
+      <NumberButton buttonValue={1} />
+      <NumberButton buttonValue={2} />
+       <NumberButton buttonValue={3} />
       <FunctionButton buttonValue="+" />
-      {/* <div className="zero-button">
+       <div className="zero-button">
         <NumberButton buttonValue={0} />
-      </div> */}
-      {/* <NumberButton buttonValue="." /> */}
-      {/* <EqualButton /> */}
+      </div>
+   {/* <EqualButton /> */}
     </div>
   </CalculatorStyles>
 );
---
+++ src/components/NumberButton.js
@@ -0,0 +1,14 @@
+
+import React, { useContext } from 'react';
+import { NumberContext } from './NumberProvider';
+
+const NumberButton = ({ buttonValue }) => {
+  const { handleSetDisplayValue } = useContext(NumberContext);
+  return (
+    <button type="button" onClick={() => handleSetDisplayValue(buttonValue)}>
+      {buttonValue}
+    </button>
+  );
+};
+
+export default NumberButton;
